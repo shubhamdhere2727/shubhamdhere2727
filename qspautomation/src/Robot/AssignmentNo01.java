f45f88faac416c25dbbac8c46c9a1d4da41e55d7
shubhamdhere2727 <shubhamdhere2727>
2022-11-08 17:51:10 +0530

    aa

---
+++ qspautomation/src/Robot/AssignmentNo01.java
@@ -0,0 +1,36 @@
+package Robot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class AssignmentNo01 {
+
+	public static void main(String[] args) throws AWTException {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://demo.guru99.com/test/drag_drop.html");
+		WebElement target = driver.findElement(By.xpath("//a[contains(@class,'button button-orange') and .=' BANK ']"));
+		Actions act = new Actions(driver);
+		act.contextClick(target).perform();;
+		
+		Robot robot = new Robot();
+		for (int i = 0; i < 10; i++) {
+			robot.keyPress(KeyEvent.VK_DOWN);
+			robot.keyRelease(KeyEvent.VK_DOWN);
+		}
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+
+	}
+
+}
